73323014acb94d848a525e346bd471e29ce10549
Mohini-2002 <77198245+Mohini-2002@users.noreply.github.com>
2023-03-20 23:19:40 +0530

    Add files via upload

---
+++ SumTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class SumTwo {
+  public static void main(String agrs[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    int n1=sc.nextInt();
+    System.out.println("Enter the second number:");
+    int n2=sc.nextInt();
+    int sum=n1+n2;
+    System.out.println("The output of two numbers is :"+sum);
+  }  
+}
